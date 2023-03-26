1edc1cf78c988cb0868ac666f2b766173160d9fb
nicsylvia <nicsylvia15f@gmail.com>
2023-03-26 15:37:46 -0700

    user models updated successfully

--- server/src/Models/UserModels.ts
+++ server/src/Models/UserModels.ts
@@ -4,43 +4,70 @@ import { UserDetails } from "../AllInterfaces/Interfaces";
 
 import isEmail from "validator/lib/isEmail";
 
-const UserSchema: Schema<UserDetails> = new Schema({
+const UserSchema: Schema<UserDetails> = new Schema(
+  {
     name: {
-        type: String,
-        required: [true, "Please enter your name"]
+      type: String,
+      required: [true, "Please enter your name"],
     },
     email: {
-        type: String,
-        required: [true, "Please enter your email"],
-        unique: true,
-        lowercase: true,
-        trim: true,
-        validate: [isEmail, "Please enter a valid email"],
+      type: String,
+      required: [true, "Please enter your email"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: [isEmail, "Please enter a valid email"],
     },
-    image:{
-        type: String,
+    image: {
+      type: String,
     },
-    username:{
-        type: String,
-        required: [true, "Please enter a suitable username"]
+    username: {
+      type: String,
+      required: [true, "Please enter a suitable username"],
     },
-    phoneNumber:{
-        type: Number,
-        required: [true, "Please enter your Phone number"]
+    phoneNumber: {
+      type: Number,
+      required: [true, "Please enter your Phone number"],
     },
     password: {
-        type: String,
-        required: [true, "Please enter your Password"]
+      type: String,
+      required: [true, "Please enter your Password"],
     },
     confirmPassword: {
-        type: String,
-        required: [true, "Please confirm your password"]
+      type: String,
+      required: [true, "Please confirm your password"],
     },
     status: {
-        type: String,
-        required: [true, "Please enter your status"],
-        message: "You must either be a User or for Business",
-        enum: ["User", "Business"],
-        default: "User"
+      type: String,
+      required: [true, "Please enter your status"],
+      message: "You must either be a User or for Business",
+      enum: ["User", "Business"],
+      default: "User",
     },
-    
+    TransactionHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Histories",
+      },
+    ],
+    // companyGiftCards:[
+    //     {
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: "GiftCards"
+    //     }
+    // ],
+    PurchasedGiftCards: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "GiftCards",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UserModels = model<UserDetails>("Users", UserSchema);
+
+export default UserModels;
