042da5f51497a5ec7f38511d7f200eca9342d805
nicsylvia <nicsylvia15f@gmail.com>
2023-03-26 15:42:28 -0700

    user registration saved

--- server/src/Controllers/UserControllers.ts
+++ server/src/Controllers/UserControllers.ts
@@ -11,3 +11,39 @@ import mongoose from "mongoose";
 import GiftCardModels from "../Models/GiftCardModels";
 import crypto from "crypto";
 import axios from "axios";
+
+// Users Registration:
+export const UsersRegistration = AsyncHandler(
+  async (req: any, res: Response, next: NextFunction) => {
+    const { name, email, phoneNumber, username, password } = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const findEmail = await UserModels.findOne({ email });
+
+    if (findEmail) {
+      next(
+        new AppError({
+          message: "User with this account already exists",
+          httpcode: HTTPCODES.FORBIDDEN,
+        })
+      );
+    }
+
+    const Users = await UserModels.create({
+      name,
+      email,
+      username,
+      phoneNumber: "234" + phoneNumber,
+      password: hashedPassword,
+      confirmPassword: hashedPassword,
+      status: "User",
+    });
+
+    return res.status(201).json({
+      message: "Successfully created User",
+      data: Users,
+    });
+  }
+);
