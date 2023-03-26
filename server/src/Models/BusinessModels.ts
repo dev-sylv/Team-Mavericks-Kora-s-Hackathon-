b420e8fb0a35bac6fa3bb1656c08c756a80b4bc7
nicsylvia <nicsylvia15f@gmail.com>
2023-03-26 15:36:13 -0700

    saved business models

--- server/src/Models/BusinessModels.ts
+++ server/src/Models/BusinessModels.ts
@@ -4,33 +4,69 @@ import { BusinessDetails } from "../AllInterfaces/Interfaces";
 
 import isEmail from "validator/lib/isEmail";
 
-const BusinessSchema: Schema<BusinessDetails> = new Schema({
+const BusinessSchema: Schema<BusinessDetails> = new Schema(
+  {
     name: {
-        type: String,
-        required: [true, "Please enter your name"],
-        unique: true
+      type: String,
+      required: [true, "Please enter your name"],
+      unique: true,
     },
     email: {
-        type: String,
-        required: [true, "Please enter your email"],
-        unique: true,
-        lowercase: true,
-        trim: true,
-        validate: [isEmail, "Please enter a valid email"],
+      type: String,
+      required: [true, "Please enter your email"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: [isEmail, "Please enter a valid email"],
     },
     logo: {
-        type: String,
+      type: String,
     },
     password: {
-        type: String,
-        required: [true, "Please enter your Password"]
+      type: String,
+      required: [true, "Please enter your Password"],
     },
     confirmPassword: {
-        type: String,
-        required: [true, "Please confirm your password"]
+      type: String,
+      required: [true, "Please confirm your password"],
     },
     BusinessCode: {
-        type: String,
-        unique: true,
+      type: String,
+      unique: true,
     },
-    
+    Balance: {
+      type: String,
+    },
+    phoneNumber: {
+      type: Number,
+      required: [true, "Please enter your Phone number"],
+      unique: true,
+    },
+    status: {
+      type: String,
+      required: [true, "Please enter your status"],
+      message: "You must either be a User or for Business",
+      enum: ["User", "Business"],
+      default: "Business",
+    },
+    giftCard: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "GiftCards",
+      },
+    ],
+    TransactionHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Histories",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const BusinessModels = model<BusinessDetails>("Businesses", BusinessSchema);
+
+export default BusinessModels;
