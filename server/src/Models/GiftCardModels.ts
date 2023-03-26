0e514710963a1fef53c2ca31f7a8045eee1ddf2a
nicsylvia <nicsylvia15f@gmail.com>
2023-03-26 15:38:54 -0700

    saved gift card models

---
+++ server/src/Models/GiftCardModels.ts
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+
+import { GiftCardDetails } from "../AllInterfaces/Interfaces";
+
+const GiftCardSchema: Schema<GiftCardDetails> = new Schema(
+  {
+    name: {
+      type: String,
+    },
+    BrandLogo: {
+      type: String,
+    },
+    uniqueID: {
+      type: String,
+    },
+    colour: {
+      type: String,
+    },
+    moneyWorth: {
+      type: Number,
+      required: [true, "Please enter the money worth of card"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const GiftCardModels = model<GiftCardDetails>("GiftCards", GiftCardSchema);
+
+export default GiftCardModels;
