d68805dca2791c9f5317df38e69f19c4528d6f5e
finbarr12 <finarrogbonna@gmail.com>
2023-03-27 11:47:33 +0100

    loginuser2

--- client/src/components/Auth/UserAuth/User.Login.tsx
+++ client/src/components/Auth/UserAuth/User.Login.tsx
@@ -42,13 +42,55 @@ const Login = () => {
             style={{ position: "absolute", bottom: "1%", left: "1px" }}
           />
         </Left>
-        <Right></Right>
+        <Right>
+          <h2>Log in</h2>
+          <Inputs>
+            <MdOutlinePersonOutline
+              style={{
+                marginLeft: "15px",
+                fontSize: "25px",
+              }}
+            />
+            <input placeholder="Enter email or username" />
+          </Inputs>
+        </Right>
       </Container>
     </div>
   );
 };
 
 export default Login;
+
+const Inputs = styled.div`
+  background-color: #f9f4ff;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  width: 430px;
+  height: 55px;
+  margin-bottom: 20px;
+  margin-left: 60px;
+
+  @media screen and (max-width: 960px) {
+    margin: 0;
+    width: 80%;
+    margin-bottom: 20px;
+  }
+
+  input {
+    flex: 1;
+    height: 100%;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    margin-left: 7px;
+
+    ::placeholder {
+      color: silver;
+      font-size: 15px;
+    }
+  }
+`;
 
 const Right = styled.div`
   h2 {
