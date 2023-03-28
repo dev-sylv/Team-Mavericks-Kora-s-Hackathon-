13c8a7b59944aefb50190840e24863f14346d84d
finbarr12 <finarrogbonna@gmail.com>
2023-03-28 22:25:52 +0100

    bizlogin

--- client/src/components/Auth/BusinessAuth/Business.Register.tsx
+++ client/src/components/Auth/BusinessAuth/Business.Register.tsx
@@ -1,4 +1,13 @@
 import React from "react";
+import styled from "styled-components";
+import rstar from "../Assets/stars.svg";
+import lstar from "../Assets/stars.svg";
+import phone from "../Assets/phone.png";
+import flower from "../Assets/flower.svg";
+import { MdOutlinePersonOutline } from "react-icons/md";
+import { IoMailOutline } from "react-icons/io5";
+import { BsTelephone } from "react-icons/bs";
+import { TbSquareKey } from "react-icons/tb";
 
 const BizzSignUp = () => {
   return <div>BizzSignUp</div>;
