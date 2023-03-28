4cdd89cb0b54a5edaf156a273e20e605177f978c
nicsylvia <nicsylvia15f@gmail.com>
2023-03-27 18:02:26 -0700

    savedall routes

--- server/src/Routes/BusinessRoutes.ts
+++ server/src/Routes/BusinessRoutes.ts
@@ -2,22 +2,39 @@ import express from "express";
 import {
   BusinessLogin,
   BusinessRegistration,
+  CheckOutToBank,
   GetSingleBusinessAcount,
+  GetSingleBusinessCards,
   UpdateBusinessLogo,
 } from "../Controllers/BusinessControllers";
+
+import {
+  BusinessRegisterValidation,
+  BusinessLoginValidation,
+} from "../Middlewares/BusinessValidation/BusinessValidation";
 
 import { BusinessLogo } from "../Config/Multer";
 
 const BusinessRouter = express.Router();
 
-BusinessRouter.route("/registerbusiness").post(BusinessRegistration);
-BusinessRouter.route("/loginbusiness").post(BusinessLogin);
+BusinessRouter.route("/registerbusiness").post(
+  BusinessRegisterValidation,
+  BusinessRegistration
+);
+BusinessRouter.route("/loginbusiness").post(
+  BusinessLoginValidation,
+  BusinessLogin
+);
 BusinessRouter.route("/getsinglebusiness/:businessID").get(
   GetSingleBusinessAcount
+);
+BusinessRouter.route("/getsinglebusiness/:businessID/cards").get(
+  GetSingleBusinessCards
 );
 BusinessRouter.route("/updatebusinesslogo/:id").patch(
   BusinessLogo,
   UpdateBusinessLogo
 );
+BusinessRouter.route("/withdraw-money/:businessID").post(CheckOutToBank);
 
 export default BusinessRouter;
--- server/src/Routes/UserRoutes.ts
+++ server/src/Routes/UserRoutes.ts
@@ -1,16 +1,26 @@
 import express from "express";
 import {
   GetSingleUser,
-  //   UserBuyAGiftCard,
+  UserBuyAGiftCardWithATMcard,
   UsersLogin,
   UsersRegistration,
 } from "../Controllers/UserControllers";
 
+import {
+  UserRegisterValidation,
+  UserLoginValidation,
+} from "../Middlewares/UserValidation/UserValidation";
+
 const UserRouter = express.Router();
 
-UserRouter.route("/registeruser").post(UsersRegistration);
-UserRouter.route("/loginuser").post(UsersLogin);
+UserRouter.route("/registeruser").post(
+  UserRegisterValidation,
+  UsersRegistration
+);
+UserRouter.route("/loginuser").post(UserLoginValidation, UsersLogin);
 UserRouter.route("/getsingleuser/:userID").get(GetSingleUser);
-// UserRouter.route("/buyagiftcard/:userID/:businessID").post(UserBuyAGiftCard);
+UserRouter.route("/buyagiftcard/:userID/:businessID/:giftcardID").post(
+  UserBuyAGiftCardWithATMcard
+);
 
 export default UserRouter;
